d4d7919499a50aa85526289ac4cbe69667bb2bbf
shaul_ei <shaul_ei@31d0aad3-184c-4fc1-91d3-241c0838d686>
2009-02-01 07:15:19 +0000

    SmartPropoPlus 2.2.0 - sources

---
+++ SPP2xx/Branches/220/SppConsole/AboutDlg.cpp
@@ -0,0 +1,68 @@
+// AboutDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "sppconsole.h"
+#include "AboutDlg.h"
+#include "..\\GlobalData\\GlobalData.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CAboutDlg dialog
+
+
+CAboutDlg::CAboutDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CAboutDlg::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CAboutDlg)
+	//}}AFX_DATA_INIT
+}
+
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CAboutDlg)
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
+	//{{AFX_MSG_MAP(CAboutDlg)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CAboutDlg message handlers
+BOOL CAboutDlg::OnInitDialog() 
+{
+		
+	/* Link to homepage */
+	m_homesite.SubclassDlgItem(IDC_WEB_LINK, this);
+	m_homesite.SetWindowText(SPP_HOME_TITLE);
+	m_homesite.SetLink(SPP_HOME_URL);
+
+	/* Main pane text */
+	CString MainText ;
+	int VerHi, VerMid, VerLo;
+	CWnd* ModTypeList = GetDlgItem(IDC_TTL_2);
+	// GUI Version
+	VerHi  = (VER_GUI&0xFF0000)>>16;
+	VerMid = (VER_GUI&0x00FF00)>>8;
+	VerLo  = (VER_GUI&0x0000FF)>>0;
+	// Compilation date
+	CTime t = CTime::GetCurrentTime();
+	CString date = t.Format("%d%b%Y");
+
+	MainText.Format(SPP_MAIN_TEXT,VerHi,VerMid,VerLo, date);
+	ModTypeList->SetWindowText(MainText);
+
+	CDialog::OnInitDialog();
+	return TRUE;  // return TRUE unless you set the focus to a control
+	              // EXCEPTION: OCX Property Pages should return FALSE
+}
---
+++ SPP2xx/Branches/220/SppConsole/resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by SppConsole.rc
+//
+#define IDD_SPPCONSOLE_DIALOG           102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU_TEST                   129
+#define IDD_ABOUT                       130
+#define IDC_MOD_TYPE                    1000
+#define IDC_SHIFT_POS                   1001
+#define IDC_SHIFT_NEG                   1002
+#define IDC_SHIFT_AUTO                  1003
+#define IDC_AUDIO_SRC                   1004
+#define IDC_HIDE                        1005
+#define IDC_WEB_LINK                    1013
+#define IDC_TTL_1                       1015
+#define IDC_TTL_2                       1016
+#define IDC_SHOW                        32771
+#define IDC_ABOUT                       32772
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1017
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
