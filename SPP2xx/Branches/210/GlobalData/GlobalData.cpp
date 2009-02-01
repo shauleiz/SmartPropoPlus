39857eb2d214b697d8349222826d2fe71d3a1002
Shaul <shauleiz@users.noreply.github.com>
2009-02-01 07:11:19 +0000

    SmartPropoPlus 2.1.0 - sources

---
+++ SPP2xx/Branches/210/GlobalData/GlobalData.cpp
@@ -0,0 +1,31 @@
+// GlobalData.cpp: implementation of the CGlobalData class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "GlobalData.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CGlobalData::CGlobalData()
+{
+
+}
+
+CGlobalData::~CGlobalData()
+{
+
+}
+
+bool CGlobalData::isValid()
+{
+	return true;
+}
