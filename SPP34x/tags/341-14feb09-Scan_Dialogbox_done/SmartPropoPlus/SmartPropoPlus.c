b64ee39092b61267cce09b98c3fabf527614b746
shaul_ei <shaul_ei@31d0aad3-184c-4fc1-91d3-241c0838d686>
2009-02-14 10:14:03 +0000

    Scan dialog box (CScanInputs) - DONE TODO: 1. Check with JR-PCM and Walkera-PPM 2. Abort operation when long scan

---
+++ SPP34x/tags/341-14feb09-Scan_Dialogbox_done/SmartPropoPlus/SmartPropoPlus.c
@@ -0,0 +1,339 @@
+/* Generic SmartPropoPlus source  file */
+#include <stdio.h>
+#include "SmartPropoPlus.h"
+//#include "GlobalMemory.h"
+#include "SppRegistry.h"
+
+void SppMessageBoxWithErrorCode(void)
+{
+	int error_code;
+	LPVOID lpMsgBuf;
+	char msg[1000];
+
+	/* Get error code and convert it to string */
+	error_code = GetLastError();
+	FormatMessage( FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_IGNORE_INSERTS,NULL, error_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),(LPTSTR) &lpMsgBuf, 0, NULL );
+	sprintf(msg, "Error(%d): %s", error_code, (LPCTSTR)lpMsgBuf);
+	MessageBox(NULL,msg, SPP_ERROR_MSG , MB_SYSTEMMODAL);
+}
+
+void SetLocationDialogWindow(int x, int y)
+{
+	SetLocationDialogWindowToRegistry(x,y);
+}
+
+BOOL GetLocationDialogWindow(int * x, int * y)
+{
+	return GetLocationDialogWindowFromRegistry(x,y);
+}
+
+//void SetNumberOfFilters(const int n)
+//{
+//	SetNumberOfFiltersToGlobalMemory(n);
+//}
+
+//int GetNumberOfFilters()
+//{
+//	return GetNumberOfFiltersFromGlobalMemory();
+//};
+//
+//void SetSelectedFilterIndex(const int i)
+//{
+//	char * name;
+//
+//	//SetSelectedFilterIndexToGlobalMemory(i);
+//	name  = GetFilterNameByIndexFromGlobalMemory(i);
+//
+//	if (i>=0 && name && strlen(name) )
+//		SetSelectedFilterNameToRegistry(name);
+//	else
+//		SetSelectedFilterNameToRegistry("");
+//}
+//
+void SetSelectedFilter(const char * FilterName)
+{
+	SetSelectedFilterNameToRegistry(FilterName);
+}
+//int GetSelectedFilterIndex()
+//{
+//	char * name;
+//	int index;
+//
+//
+//	/* Not found - get name of selected filter from registry and convert to index */
+//	name = GetSelectedFilterNameFromRegistry();
+//	if (name && strlen(name))
+//		index  = GetFilterIndexByNameFromGlobalMemory(name);
+//	else
+//		/* Get the index of the selected filter from global memory */
+//		index  = GetSelectedFilterIndexFromGlobalMemory();
+//
+//	/* Update the global memory */
+//	SetSelectedFilterIndexToGlobalMemory(index);
+//	return index;
+//}
+//
+//
+//void SetFilterNames(const char ** name)
+//{
+//	SetFilterNamesToGlobalMemory(name);
+//}
+//char * GetFilterNameByIndex(const int i)
+//{
+//	return GetFilterNameByIndexFromGlobalMemory(i);
+//}
+
+
+
+/*
+	Set the current active modulation type
+*/
+int SetActiveMode(const char * selected)
+{
+	//SetActiveModeToGlobalMemory(selected);
+	return SetActiveModeToRegistry(selected);
+}
+
+/*
+	Set the current shift auto-detect value
+*/
+void SetShiftAutoDetect(const int sel)
+{
+	//SetShiftAutoDetectToGlobalMemory(sel);
+	SetShiftAutoDetectToRegistry(sel);
+}
+
+/*
+	Set the current shift polarity value
+*/
+void SetPositiveShift(const int sel)
+{
+	//SetPositiveShiftToGlobalMemory(sel);
+	SetPositiveShiftToRegistry(sel);
+}
+
+
+/*
+	Get modulation data.
+	First try the global data. If does not exist get it from the registry
+*/
+struct Modulations * GetModulation(int Create)
+{
+	//struct Modulations * Out;
+
+	//Out =  GetModulationFromGlobalMemory();
+	//if (Out)
+	//	return Out;
+	//else
+		return GetModulationFromRegistry(Create);
+}
+
+/*
+	Get debug level
+*/
+int GetDebugLevel(void)
+{
+		return GetDebugLevelFromRegistry();
+}
+
+
+const char * ModeDisplayFromInternalName(const char * internal)
+{
+	int i=0;
+	const char *DefMods[] = MOD_DEF_STR;
+
+	while (DefMods[i*2])
+	{
+		if (!strcmp(DefMods[i*2], internal))
+			return DefMods[i*2 + 1];
+		i++;
+	};
+
+	return "******";
+}
+
+/* Test existence of Audi2Joystick registry keys */
+int isBaseRegistryExist()
+{
+
+	long res;
+	HKEY hkResult;
+
+	res = RegOpenKeyEx(HKEY_CURRENT_USER,REG_BASE, 0, KEY_READ, &hkResult);
+	if (res != ERROR_SUCCESS)
+	{
+		SetLastError(res);
+		return 0;
+	};
+
+	RegCloseKey(hkResult);
+	return 1;
+}
+
+
+/*
+	Get default value (normalized) of volume per source type (Mike, Line In etc.)
+*/
+int GetDefaultVolumeValue(unsigned long SrcType)
+{
+	unsigned long VolumeValue;
+
+	// Get the needed value from the registry
+	if (GetDefaultVolumeValueFromRegistry(SrcType, &VolumeValue))
+		return VolumeValue;
+
+	// Calculate the needed volume
+	if (SrcType == MIXERLINE_COMPONENTTYPE_SRC_MICROPHONE)
+		VolumeValue = TX_VOLUME_MIC;
+	else if (SrcType == MIXERLINE_COMPONENTTYPE_SRC_LINE)
+		VolumeValue = TX_VOLUME_LINE;
+	else if (SrcType == MIXERLINE_COMPONENTTYPE_SRC_AUXILIARY)
+		VolumeValue = TX_VOLUME_AUX;
+	else if (SrcType == MIXERLINE_COMPONENTTYPE_SRC_ANALOG)
+		VolumeValue = TX_VOLUME_ANALOG;
+	else
+		VolumeValue = TX_VOLUME_UNKNOWN;
+
+	// Record this value in the registry
+	SetDefaultVolumeValueToRegistry(SrcType, VolumeValue);
+
+	return VolumeValue;
+}
+
+/*
+	Set default value (normalized) of volume per source type (Mike, Line In etc.)
+*/
+int SetDefaultVolumeValue(unsigned long SrcType, unsigned long  VolumeValue)
+{
+	return SetDefaultVolumeValueToRegistry(SrcType, VolumeValue);
+}
+
+int GetCurrentAudioState()
+{
+	int AudioState;
+	//AudioState = GetCurrentAudioStateFromGlobalMemory();
+	//if (AudioState>=0)
+	//	return AudioState;
+	//else
+	//{
+		AudioState =  GetCurrentAudioStateFromRegistry();
+	//	SetCurrentAudioStateToGlobalMemory(AudioState);
+		return AudioState;
+	//}
+}
+
+void SetCurrentAudioState(int Active)
+{
+	SetCurrentAudioStateToRegistry(Active);
+	//SetCurrentAudioStateToGlobalMemory(Active);
+}
+
+int GetCurrentPpjoyState()
+{
+	return GetCurrentPpjoyStateFromRegistry();
+}
+
+void SetCurrentPpjoyState(int Active)
+{
+	SetCurrentPpjoyStateToRegistry(Active);
+}
+
+char * GetCurrentMixerDevice()
+{
+	char * MixerName;
+	int AudioState;
+
+	AudioState = GetCurrentAudioState();
+
+	if (AudioState)
+	{ /* Checkbox "Enable Audio input selection" is checked */
+		//MixerName = GetCurrentMixerDeviceFromGlobalMemory();
+		//if (!MixerName || !strlen(MixerName))
+		MixerName =  GetCurrentMixerDeviceFromRegistry();
+		return MixerName;
+	}
+	else
+		return NULL;
+}
+
+int GetCurrentInputLine(unsigned int *SrcID)
+{
+	return GetCurrentInputLineFromRegistry(SrcID);
+}
+
+int GetWaveInParams(int *nSamples, int *nBits, int *nBuffers, int *BufferSize)
+{
+	return GetWaveInParamsFromRegistry(nSamples, nBits, nBuffers, BufferSize);
+}
+void SetCurrentMixerDevice(const char * MixerName)
+{
+	SetCurrentMixerDeviceToRegistry(MixerName);
+	//SetCurrentMixerDeviceToGlobalMemory(MixerName);
+}
+
+void SetCurrentInputLine(const char * MixerName, unsigned int SrcID)
+{
+	SetCurrentInputLineToRegistry(MixerName, SrcID);
+}
+
+//far void * CreateDataBlock(struct Modulations * data)
+//{
+//	return  CreateSharedDataStruct(data);
+//}
+
+int GetAntiJitterState()
+{
+	return GetAntiJitterStateFromRegistry();
+}
+
+void SetAntiJitterState(int enable)
+{
+	SetAntiJitterStateToRegistry(enable);
+}
+
+int GetSppConsoleFullPath(char * Path, UINT PathSize)
+{
+	return GetSppConsoleFullPathFromRegistry(Path,PathSize);
+}
+
+/* 
+	Get the Fms.exe full path in 'Path'
+	Return:
+		1	If Both Fms.exe & Winmm.dll are located in the FMS folder
+		0	If FMS not installed
+		-1	If Winmm.dll is missing
+		-2	If FMS instaled but Fms.exe is missing
+*/
+int GetFmsFullPath(char * Path, UINT PathSize)
+{
+	int GetFmsStat;
+	char TmpPath[MAX_PATH+1] = "";
+	HANDLE  hFile;
+	WIN32_FIND_DATA FileData;
+
+	if (!Path)
+		return 0;
+
+	/* Get the FMS directory from the registry */
+	GetFmsStat = GetFmsFullPathFromRegistry(&(TmpPath[0]),PathSize);
+	if (!GetFmsStat)
+		return 0;
+
+	/* Check that file winmm.dll exists in the FMS directory */
+	sprintf(Path,"%s\\Winmm.dll", TmpPath);
+	hFile = FindFirstFile((LPCTSTR)Path, &FileData);
+	if (hFile == INVALID_HANDLE_VALUE)
+		return -1;
+	FindClose(hFile);
+
+	/* Check that file Fms.exe exists in the FMS directory */
+	sprintf(Path,"%s\\Fms.exe", TmpPath);
+	hFile = FindFirstFile((LPCTSTR)Path, &FileData);
+	if (hFile == INVALID_HANDLE_VALUE)
+		return -2;
+	FindClose(hFile);
+
+
+	return 1;
+}
+ 
---
+++ SPP34x/tags/341-14feb09-Scan_Dialogbox_done/SppConsole/DynamicLED.cpp
@@ -0,0 +1,336 @@
+// DynamicLED.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "DynamicLED.h"
+
+#define ID_TIMER_START 1001
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CDynamicLED
+
+CDynamicLED::CDynamicLED()
+{
+	// Initialize the variables
+	m_bBright = FALSE;
+	m_nTimerInterval = 0;
+	m_nPrevTimerInterval = 0;
+	m_pdc = NULL;
+	m_BlinkTimer = 0;
+	m_hWnd = NULL;
+}
+
+CDynamicLED::~CDynamicLED()
+{
+	if (m_pdc) delete m_pdc;
+}
+
+BEGIN_MESSAGE_MAP(CDynamicLED, CStatic)
+	//{{AFX_MSG_MAP(CDynamicLED)
+	ON_WM_PAINT()
+	ON_WM_TIMER()
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+
+/*
+	Initialize led to shape & colour
+	Set of Off
+	Input:
+		pWnd:		Pointer to object where this control is located
+		nIDColor:	LED colour. Possible values: ID_LED_RED/ID_LED_GREEN/ID_LED_BLUE/ID_LED_YELLOW
+		nIDShape:	Led shape.  Possible values: ID_SHAPE_ROUND/ID_SHAPE_SQUARE
+	No Return Value
+*/
+void CDynamicLED::InitLed(CWnd *pWnd, UINT nIDColor, UINT nIDShape)
+{
+	m_hWnd = pWnd->m_hWnd;
+
+	// Save the color,shape and the timer interval of the control as it 
+	// will be used later when resetting the LED to its previous state.
+	m_nID = nIDColor;
+	m_nShape = nIDShape;
+
+	if (!m_pdc) m_pdc = new CClientDC(this);
+	SetPenBrush();
+
+	SwitchOff();
+	return;
+}
+
+/*
+	Paint event handler
+*/
+void CDynamicLED::OnPaint() 
+{
+	CPaintDC dc(this); // device context for painting
+
+	Switch(m_bBright);
+	return;
+}
+
+/*
+	Timer event handler
+	Used for blinking
+*/
+void CDynamicLED::OnTimer(UINT nIDEvent) 
+{	
+	SetLedBright(NULL,TRUE);
+
+	// Get the rectangle of the window where we are going to draw
+	CRect rcClient;
+	GetClientRect(&rcClient);
+
+	// Draw the LED (Round/Square)
+	if(m_nShape==ID_SHAPE_ROUND) DrawRoundLed(rcClient);
+	else if(m_nShape==ID_SHAPE_SQUARE) DrawSquareLed(rcClient);
+
+	CStatic::OnTimer(nIDEvent);
+}
+
+/*
+	Turn the led on/off 
+	On:		Bright colour
+	Off:	Dark colour
+*/
+void CDynamicLED::Switch(BOOL on)
+{
+	if (m_BlinkTimer) 
+	{
+		KillTimer(m_BlinkTimer);
+		m_BlinkTimer = 0;
+	};
+
+	SetLedBright(on,FALSE);
+
+	// Get the rectangle of the window where we are going to draw
+	CRect rcClient;
+	GetClientRect(&rcClient);
+
+	// Draw the LED (Round/Square)
+	if(m_nShape==ID_SHAPE_ROUND) DrawRoundLed(rcClient);
+	else if(m_nShape==ID_SHAPE_SQUARE) DrawSquareLed(rcClient);
+
+	return;
+}
+
+/*
+	Turn the led ON (Bright colour)
+*/
+void CDynamicLED::SwitchOn()
+{
+	Switch(TRUE);
+}
+/*
+	Turn the led OFF (Dark colour)
+*/
+void CDynamicLED::SwitchOff()
+{
+	Switch(FALSE);
+}
+
+/*
+	Set LED to blink (Alternate bright/dark colour)
+	Input Value: milisec - blink period in miliseconds
+*/
+void CDynamicLED::Blink(int milisec)
+{
+	static rate=0;
+
+	if (!rate)
+		rate = milisec;
+	else if (m_BlinkTimer && milisec == rate)
+		return;
+
+	if (m_BlinkTimer) KillTimer(m_BlinkTimer);
+	m_BlinkTimer = SetTimer(ID_TIMER_START,milisec,NULL);
+}
+
+/*
+	Set LED to Bright/Dark if 'Toggle'=FALSE (Default)
+	If Toggle=TRUE then toggle brightness (Ignor value of 'Bright')
+*/
+BOOL CDynamicLED::SetLedBright(BOOL Bright, BOOL Toggle)
+{
+	if (!m_pdc)
+		return FALSE;
+
+	// Save previous value
+	BOOL ret = m_bBright;
+
+	// Set brightness
+	if (Toggle && m_bBright) m_bBright = FALSE;
+	else if (Toggle && !m_bBright) m_bBright = TRUE;
+	else if (Bright) m_bBright = TRUE;
+	else m_bBright = FALSE;
+
+	if(m_bBright==TRUE)
+	{
+		// If we have to switch on the LED to display the bright colour select
+		// the bright pen and brush that we have created above
+
+		m_pdc->SelectObject(&m_PenBright);
+		m_pdc->SelectObject(&m_BrushBright);
+
+		m_BrushCurrent.m_hObject = m_BrushBright.m_hObject;
+	}
+	else
+	{
+		// If we have to switch off the LED to display the dark colour select
+		// the bright pen and brush that we have created above
+
+		m_pdc->SelectObject(&m_PenDark);
+		m_pdc->SelectObject(&m_BrushDark);
+
+		m_BrushCurrent.m_hObject = m_BrushDark.m_hObject;
+	}
+
+	return ret;
+}
+
+/*
+	Helper function - Set the pens and brushes to a set of bright/dark colours
+*/
+void CDynamicLED::SetPenBrush(void)
+{
+	// If the pen has been selected already, then we have to delete it
+	// so that it doesnt throw an assertion
+
+	if(m_PenBright.m_hObject!=NULL) m_PenBright.DeleteObject();		// Bright pen
+	if(m_BrushBright.m_hObject!=NULL) m_BrushBright.DeleteObject();	// Bright brush
+	if(m_PenDark.m_hObject!=NULL) m_PenDark.DeleteObject();			// Dark pen
+	if(m_BrushDark.m_hObject!=NULL)	m_BrushDark.DeleteObject();		// Dark brush
+
+
+	// If the user has selected RED as the color of the LED
+	if(m_nID==ID_LED_RED)
+	{
+		// I'm creating a light shade of red here for displaying the bright
+		// LED. You can change the values to any colour that you want
+		m_PenBright.CreatePen(0,1,RGB(250,0,0));
+		m_BrushBright.CreateSolidBrush(RGB(250,0,0));
+
+		// Here i'm creating a dark shade of red. You can play with the values to
+		// see the effect on the LED control
+
+		m_PenDark.CreatePen(0,1,RGB(150,0,0));
+		m_BrushDark.CreateSolidBrush(RGB(150,0,0));
+	}
+	else if(m_nID==ID_LED_GREEN)
+	{
+		// If the user has selected GREEN as the color of the LED
+
+		m_PenBright.CreatePen(0,1,RGB(0,250,0));
+		m_BrushBright.CreateSolidBrush(RGB(0,250,0));
+
+		m_PenDark.CreatePen(0,1,RGB(0,150,0));
+		m_BrushDark.CreateSolidBrush(RGB(0,150,0));
+	}
+	else if(m_nID==ID_LED_BLUE)
+	{
+		// If the user has selected BLUE as the color of the LED
+
+		m_PenBright.CreatePen(0,1,RGB(0,0,250));
+		m_BrushBright.CreateSolidBrush(RGB(0,0,250));
+
+		m_PenDark.CreatePen(0,1,RGB(0,0,150));
+		m_BrushDark.CreateSolidBrush(RGB(0,0,150));
+	}
+	else if(m_nID==ID_LED_YELLOW)
+	{
+		// If the user has selected YELLOW as the color of the LED
+
+		m_PenBright.CreatePen(0,1,RGB(200,200 ,0));
+		m_BrushBright.CreateSolidBrush(RGB(200,200,0));
+
+		m_PenDark.CreatePen(0,1,RGB(150,150,0));
+		m_BrushDark.CreateSolidBrush(RGB(150,150,0));
+	}
+
+}
+
+/*
+	Draw a round LED within the given rectangle of the window where we are going to draw
+*/
+void CDynamicLED::DrawRoundLed(CRect rcClient)
+{
+	if (!m_pdc)
+		return;
+
+	// Draw the actual colour of the LED
+	//m_pdc->Ellipse(rcClient);
+
+	// Draw a thick dark gray coloured circle
+	CPen Pen;
+	Pen.CreatePen(0,2,RGB(128,128,128));
+	m_pdc->SelectObject(&Pen);
+
+	m_pdc->Ellipse(rcClient);
+
+	// Draw a thin light gray coloured circle
+	Pen.DeleteObject();
+	Pen.CreatePen(0,1,RGB(192,192,192));
+	m_pdc->SelectObject(&Pen);
+	m_pdc->Ellipse(rcClient);
+
+	// Draw a white arc at the bottom
+	Pen.DeleteObject();
+	Pen.CreatePen(0,1,RGB(255,255,255));
+	m_pdc->SelectObject(&Pen);
+
+	// The arc function is just to add a 3D effect for the control
+	CPoint ptStart,ptEnd;
+	ptStart = CPoint(rcClient.Width()/2,rcClient.bottom);
+	ptEnd	= CPoint(rcClient.right,rcClient.top);
+
+	m_pdc->MoveTo(ptStart);
+	m_pdc->Arc(rcClient,ptStart,ptEnd);
+
+	CBrush Brush;
+	Brush.CreateSolidBrush(RGB(255,255,255));
+	m_pdc->SelectObject(&Brush);
+
+	// Draw the actual ellipse
+	m_pdc->Ellipse(rcClient.left+4,rcClient.top+4,rcClient.left+6,rcClient.top+6);
+}
+
+
+/*
+	Draw a rectangular LED within the given rectangle of the window where we are going to draw
+*/
+void CDynamicLED::DrawSquareLed(CRect rcClient)
+{
+	if (!m_pdc)
+		return;
+
+	// Draw the actual rectangle
+	m_pdc->FillRect(rcClient,&m_BrushCurrent);
+
+	// The  code below gives a 3D look to the control. It does nothing more
+
+	// Draw the dark gray lines
+	CPen Pen;
+	Pen.CreatePen(0,1,RGB(128,128,128));
+	m_pdc->SelectObject(&Pen);
+
+	m_pdc->MoveTo(rcClient.left,rcClient.bottom);
+	m_pdc->LineTo(rcClient.left,rcClient.top);
+	m_pdc->LineTo(rcClient.right,rcClient.top);
+
+	Pen.DeleteObject();
+
+	// Draw the light gray lines
+	Pen.CreatePen(0,1,RGB(192,192,192));
+	m_pdc->SelectObject(&Pen);
+
+	m_pdc->MoveTo(rcClient.right,rcClient.top);
+	m_pdc->LineTo(rcClient.right,rcClient.bottom);
+	m_pdc->LineTo(rcClient.left,rcClient.bottom);	
+}
+
