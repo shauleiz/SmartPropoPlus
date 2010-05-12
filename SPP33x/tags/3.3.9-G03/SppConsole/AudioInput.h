f671031ab1dbbf8c204527d94ac73f6179598ba5
Shaul <shauleiz@users.noreply.github.com>
2010-05-12 05:32:29 +0000

    Functional, still need minor fixes. Main problem - cannot mute noise. Plan: Move to Device Topology approach in order to gain better control on the internal components (Vista/W7).

---
+++ SPP33x/tags/3.3.9-G03/SppConsole/AudioInput.h
@@ -0,0 +1,140 @@
+// AudioInput.h: interface for the CAudioInput class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_AUDIOINPUT_H__9FACD8AD_2945_4282_9FA6_F366CCBA46BD__INCLUDED_)
+#define AFX_AUDIOINPUT_H__9FACD8AD_2945_4282_9FA6_F366CCBA46BD__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+#include <Mmsystem.h>
+/* Global functions */
+long GetSelSrcLineID(HMIXER hMixerDevice, unsigned int DstLineID);
+MMRESULT GetLineControlList(HMIXEROBJ hMixerDevice, unsigned long LineID, MIXERCONTROL * ControlList, int * cControls=NULL);
+MMRESULT GetControlDetailList(HMIXER hmxobj, LPMIXERCONTROLDETAILS_LISTTEXT list, DWORD cMultipleItems, unsigned int dwControlID);
+MMRESULT GetControlDetailList(HMIXER hmxobj, LPMIXERCONTROLDETAILS_BOOLEAN list, DWORD cMultipleItems, unsigned int dwControlID);
+bool GetVolumeValue(HMIXER hMixerDevice, unsigned int ControlID, long * value);
+int SetVolumeValue(HMIXER hMixerDevice, unsigned int ControlID, long  value);
+MMRESULT GetLineControlByID(HMIXEROBJ hMixerDevice, unsigned long ControlID, MIXERCONTROL * Control);
+bool isSpk(unsigned int type);
+bool GetMuteValue(HMIXER hMixerDevice, unsigned int ControlID, bool * value);
+MMRESULT Mute(HMIXER hMixerDevice, unsigned int ControlID, bool value=true);
+MMRESULT SetControlDetailList(HMIXER hmxobj, LPMIXERCONTROLDETAILS_BOOLEAN list, DWORD cMultipleItems, unsigned int dwControlID);
+MMRESULT SetControlDetailList(HMIXER hmxobj, LPMIXERCONTROLDETAILS_UNSIGNED list, DWORD cMultipleItems, unsigned int dwControlID);
+
+
+class CAudioInput  
+{
+protected: 
+	class CMixerDevice
+	{
+	protected:
+		class CPhysicalDevice
+		{
+		public: //  CPhysicalDevice
+			virtual ~CPhysicalDevice();
+			CPhysicalDevice();
+			CPhysicalDevice(HMIXER hMixerDevice, unsigned long DstLineID, unsigned long muteCtrlID,bool mute, unsigned long volumeCtrlID, int volume);
+			void SetVolume(int volume=50);
+			void SetMute(bool mute=true);
+			void restore();
+		protected: //  CPhysicalDevice
+			unsigned long m_DstLineID;
+			unsigned long m_muteCtrlID;
+			unsigned long m_volumeCtrlID;
+			bool m_mute;
+			int m_volume;
+			HMIXER m_hMixerDevice;
+		};//  CPhysicalDevice
+
+		class CInputLine
+		{
+		struct sMuteLine {bool mute; ULONG LineID; ULONG CtrlID;};
+		public: // CInputLine
+			int StoreDefaultVolume();
+			bool isInitSelected(void);
+			void RestoreMute(void);
+			bool SetMute(bool mute=true, bool temporary = false);
+			int RestoreVolume(void);
+			void SetDefaultVolume(void);
+			unsigned long GetSrcID(void);
+			const char * GetName(void);
+			void SetSrcType(unsigned long type);
+			void SetName(const char * name);
+			CInputLine();
+			CInputLine(HMIXER hMixerDevice, unsigned long id, const char * Name, unsigned long Type, long Selected);
+			virtual ~CInputLine();
+
+		protected: // CInputLine
+			HMIXER m_hMixerDevice;
+			unsigned long m_ID;
+			const char * m_Name;
+			unsigned long m_SrcType;
+			int	m_InitVolume;
+			bool m_InitSelected;
+			CArray< sMuteLine ,sMuteLine> * m_ArrayMuteControl;
+			CArray< sMuteLine ,sMuteLine>  * CreateArrayMuteCtrl(ULONG m_SrcType, const char * m_Name);
+		}; // CInputLine
+
+
+		public: //CMixerDevice
+			int SetSpeakers(bool restore=false, bool mute=true);
+			bool MuteSelectedInputLine(unsigned int line, bool mute=true, bool temporary=false);
+			void RestoreSelectedInputLine(unsigned int line);
+			bool GetInputLineSrcID(unsigned int * SrcID, unsigned int Index);
+			bool GetInputLineIndex(unsigned int SrcID, unsigned int * Index);
+			bool GetSelectedInputLine(unsigned int * iLine);
+			int Restore(void);
+			void SelectInputLine(unsigned int SrcID);
+			void AdjustInputVolume(unsigned long SrcID);
+			bool MuteOutputLine(unsigned long SrcID, bool mute=true, bool temporary=false);
+			bool SetSelectedInputLine(unsigned int line);
+			const char * GetInputLineName(int i);
+			const char * GetName();
+			virtual ~CMixerDevice();
+			CMixerDevice(int Index);
+
+		protected: //CMixerDevice
+			CMixerDevice();
+			int CreatePhysicalDevArray(void);
+			int CreateInputLineArray(void);
+
+		protected: //CMixerDevice
+			HMIXER m_hMixerDevice;
+			int m_ID;
+			char * m_name;
+			long m_CurrentSrcID;
+			CArray< CInputLine *,CInputLine *> m_ArrayInputLine;
+			CArray< CPhysicalDevice *,CPhysicalDevice *> m_ArrayPhysicalDev;
+	}; // CMixerDevice
+	
+
+	public: // CAudioInput
+		virtual int GetCurrentMixerDevice(void);
+		virtual bool SetCurrentMixerDevice(int i);
+		virtual int GetMixerDeviceIndex(char * mixer);
+		virtual void Restore(void);
+		virtual const char * GetMixerDeviceName(int index);
+		virtual const char * GetMixerDeviceInputLineName(int Mixer, int Line);
+		virtual bool GetMixerDeviceInputLineSrcID(int Mixer, unsigned int * SrcID, unsigned int Index);
+		virtual bool GetMixerDeviceInputLineIndex(int Mixer, unsigned int SrcID, unsigned int * Index);
+		virtual bool SetMixerDeviceSelectInputLine(int Mixer, int Line);
+		virtual bool GetMixerDeviceSelectInputLine(int Mixer, unsigned int * iLine);
+		virtual bool MuteSelectedInputLine(int Mixer, unsigned int line, bool mute=true, bool temporary=false);
+		virtual int SetSpeakers(int Mixer, bool restore=false, bool mute=true);
+		virtual int GetCountMixerDevice(void);
+		CAudioInput();
+		CAudioInput(bool dummy);
+		virtual ~CAudioInput();
+		
+	protected: // CAudioInput
+		CMixerDevice * GetMixerDevice(int Mixer);
+
+	protected: // CAudioInput
+		CArray< CMixerDevice *,CMixerDevice *> m_ArrayMixerDevice;
+		int m_CurrentMixerDevice;
+}; // CAudioInput
+
+
+#endif // !defined(AFX_AUDIOINPUT_H__9FACD8AD_2945_4282_9FA6_F366CCBA46BD__INCLUDED_)
---
+++ SPP33x/tags/3.3.9-G03/SppConsole/AudioInputW7.cpp
@@ -0,0 +1,213 @@
+#include "stdafx.h"
+#include ".\audioinputw7.h"
+
+
+
+/***************************************************************************************
+	Class CAudioInputW7 (Derived from CAudioInput)
+
+	Object represents the Vista/Windows7 audio system (single object)
+	members consist of list of Endpoint devices & index of current Endpoint device
+	Endpoint devices are also called Mixer (devices)
+***************************************************************************************/
+/////////////////// Helper functions //////////////////////////////////////////////////
+
+int w2char(LPWSTR wIn, char * cOut, int size)
+{
+	int i;
+	for (i=0; i<size ; i++)
+	{
+		cOut[i] = (char)wIn[i];
+		if (!cOut[i])
+			break;
+	};
+	return i;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+CAudioInputW7::CAudioInputW7(void) 
+#ifndef STANDALONE
+: CAudioInput(false)
+#endif
+{
+	HRESULT hr = S_OK;
+
+
+	/* Initializations */
+	m_pEnumerator = NULL;
+	m_pDeviceCollect = NULL;
+	m_nEndPoints = 0;
+	m_ArrayOfEndPointNames = NULL;
+
+
+	//BCDE0395-E52F-467C-8E3D-C4579291692E
+	static GUID const CLSID_MMDeviceEnumerator = {
+     0xBCDE0395, 0xE52F, 0x467C, {0x8E,0x3D,0xC4,0x57,0x92,0x91,0x69,0x2E} };
+
+
+	 //{A95664D2-9614-4F35-A746-DE8DB63617E6}
+	static GUID const CLSID_IMMDeviceEnumerator = {
+     0xA95664D2, 0x9614, 0x4F35, {0xA7,0x46,0xDE,0x8D,0xB6,0x36,0x17,0xE6} };
+
+	 //{1CB9AD4C-DBFA-4C32-B178-C2F568A703B2}
+	static GUID const IID_IAudioClient = {
+     0x1CB9AD4C, 0xDBFA, 0x4C32, {0xB1,0x78,0xC2,0xF5,0x68,0xA7,0x03,0xB2} };
+
+	//{C8ADBD64-E71E-48A0-A4DE-185C395CD317}
+	static GUID const IID_IAudioCaptureClient = {
+     0xC8ADBD64, 0xE71E, 0x48A0, {0xA4,0xDE,0x18,0x5C,0x39,0x5C,0xD3,0x17} };
+
+	CoInitializeEx(NULL, COINIT_MULTITHREADED);
+	//_ASSERTE(0);
+
+	/* Create a device enumarator then a collection of endpoints and finally get the number of endpoints */
+	hr = CoCreateInstance(CLSID_MMDeviceEnumerator, NULL, CLSCTX_ALL, CLSID_IMMDeviceEnumerator, (void**)&m_pEnumerator);
+	if (FAILED(hr))
+	{
+		MessageBox(NULL,"WASAPI (CAudioInputW7): Could not find default audio Endpoint\r\nStopping audio capture", "SmartPropoPlus Message" , MB_SYSTEMMODAL|MB_ICONERROR);
+		EXIT_ON_ERROR(hr);
+	};
+
+	// Create a list of Endpoints
+	hr = m_pEnumerator->EnumAudioEndpoints(eCapture, DEVICE_STATE_ACTIVE, &m_pDeviceCollect);
+	if (FAILED(hr))
+	{
+		MessageBox(NULL,"WASAPI (CAudioInputW7): Could not enumerate audio Endpoint\r\nStopping audio capture", "SmartPropoPlus Message" , MB_SYSTEMMODAL|MB_ICONERROR);
+		EXIT_ON_ERROR(hr);
+	};
+
+	hr = m_pDeviceCollect->GetCount(&m_nEndPoints);
+	if (FAILED(hr))
+	{
+		MessageBox(NULL,"WASAPI (CAudioInputW7): Could not count audio Endpoints\r\nStopping audio capture", "SmartPropoPlus Message" , MB_SYSTEMMODAL|MB_ICONERROR);
+		EXIT_ON_ERROR(hr);
+	};
+
+	bool created = CreateArrayOfEndPointNames();
+
+	return;
+
+	Exit:
+    SAFE_RELEASE(m_pEnumerator);
+    SAFE_RELEASE(m_pDeviceCollect);
+
+
+}
+
+CAudioInputW7::~CAudioInputW7(void)
+{
+    SAFE_RELEASE(m_pEnumerator);
+    SAFE_RELEASE(m_pDeviceCollect);
+	delete[] m_ArrayOfEndPointNames;
+}
+
+int CAudioInputW7::GetCountMixerDevice()
+{
+	return m_nEndPoints;
+};
+
+const char * CAudioInputW7::GetMixerDeviceName(int index)
+{
+	// Sanity checks
+	if ((UINT)index >= m_nEndPoints || index<0 || !m_ArrayOfEndPointNames)
+		return "";
+
+	return m_ArrayOfEndPointNames[index];
+}
+
+
+bool CAudioInputW7::CreateArrayOfEndPointNames(void)
+{
+	HRESULT hr = S_OK;
+	IMMDevice *pDeviceIn = NULL;
+	PROPVARIANT varName;
+	IPropertyStore *pProps = NULL;
+	IAudioClient * pClientIn = NULL;
+	LPWSTR pwszID = NULL;
+	bool Ret = false;
+
+	
+	// Sanity check
+	if (!m_pDeviceCollect)
+		return false;
+
+	// Create an array of Endpoint names only if 
+	//	Array does not exist
+	//	There is at least one endpoint
+	if (m_ArrayOfEndPointNames || !m_nEndPoints)
+		return false;
+
+	m_ArrayOfEndPointNames = new char*[m_nEndPoints];
+
+	for (UINT index=0; index<m_nEndPoints ; index++)
+	{
+		// Get pointer to endpoint number i.
+		hr = m_pDeviceCollect->Item(index, &pDeviceIn);
+		EXIT_ON_ERROR(hr);
+
+		// Get the endpoint ID string.
+		hr = pDeviceIn->GetId( &pwszID);
+		EXIT_ON_ERROR(hr);
+
+		// TODO
+		//// Test if device is usable
+		// Activate
+		//hr = pDeviceIn->Activate(&IID_IAudioClient, CLSCTX_ALL,  NULL, (void**)&pClientIn);
+		//EXIT_ON_ERROR(hr);
+
+
+
+		hr = pDeviceIn->OpenPropertyStore(STGM_READ, &pProps);
+		EXIT_ON_ERROR(hr);
+
+		// Initialize container for property value.
+		PropVariantInit(&varName);
+
+		// Get the endpoint's friendly-name property.
+		hr = pProps->GetValue(PKEY_Device_FriendlyName, &varName);
+		EXIT_ON_ERROR(hr);
+
+		// Convert to char
+		size_t size = wcslen(varName.pwszVal);
+		m_ArrayOfEndPointNames[index] = new char[size+2];
+		w2char(varName.pwszVal, m_ArrayOfEndPointNames[index], (int)size+1);
+	};
+
+	Ret=true;
+
+Exit:
+	SAFE_RELEASE(pDeviceIn);
+    SAFE_RELEASE(pProps);
+	return Ret;
+}
+
+int CAudioInputW7::GetMixerDeviceIndex(char * mixer)
+{
+	if (!m_ArrayOfEndPointNames)
+		return -1;
+
+	for (UINT index=0; index<m_nEndPoints ; index++)
+	{
+		if (!strcmp(m_ArrayOfEndPointNames[index], mixer))
+			return index;
+	}
+
+	return -1;
+}
+
+const char * CAudioInputW7::GetMixerDeviceInputLineName(int Mixer, int Line)
+{
+	return NULL;
+}
+
+bool CAudioInputW7::GetMixerDeviceSelectInputLine(int Mixer, unsigned int * iLine)
+{
+	*iLine = -1;
+	return false;
+}
+
+bool  CAudioInputW7::SetMixerDeviceSelectInputLine(int Mixer, int Line)
+{
+	return false;
+}
