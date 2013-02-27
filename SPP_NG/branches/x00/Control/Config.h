3e54fff671809361fc3683bed891b2a109d17dd6
Shaul <shauleiz@users.noreply.github.com>
2013-02-27 20:19:00 +0000

    Class CConfig is used for interfacing with the configuration XML files.

---
+++ SPP_NG/branches/x00/Control/Config.h
@@ -0,0 +1,79 @@
+#pragma once
+using namespace std;
+#include <tinyxml.h>
+#include <tinystr.h>
+
+///////////////////////////////////////////////////////////////////////////
+#define	STR_CTRL_XML		L"\\SmartPropoPlus\\Control.xml"
+#define	STR_DEFLT_CTRL_XML	L".\\Control.xml"
+#define	XML_ROOT_ELEM		L"SmartPropoPlus"
+#define	XML_AUDIO_ELEM		"Audio"
+#define	XML_CEP_ELEM		"CaptureEndPoint"
+#define	XML_ID_ELEM			"id"
+#define	XML_COLOR_ELEM		"color"
+#define	XML_FNAME_ELEM		"friendlyName"
+#define	XML_CSTAT_ELEM		"currentState"
+#define	XML_ENBLD_ELEM		"Enabled"
+#define	XML_CHNLS_ELEM		"Channels"
+#define	XML_SELEP_ELEM		"SelectedEndPoint"
+#define	XML_ORIG_ELEM		"Original"
+#define	XML_CURR_ELEM		"Current"
+#define	XML_DCDRS_ELEM		"Decoders"
+#define	XML_DCDR_ELEM		"Decoder"
+#define	XML_VER_ELEM		"ver"
+
+typedef std::map<std::string,std::string> MessageMap;
+
+struct StateCapEP
+{
+	char Enabled[4]; // Yes/No
+	char Channels[4]; // 2,3,4,5.1,7.1
+};
+
+struct CaptureEndPoint
+{	// Reflect element <CaptureEndPoint>
+	char friendlyName[MAX_PATH]; // UTF-8
+	char id[MAX_PATH]; // Ex: {0.0.1.00000000}{E3DCE116-D402-4b6a-842F-4FDD238FB870}
+	char color[12];		// Ex: 0x00FF8080
+	StateCapEP currentState;
+	StateCapEP originalState;
+};
+
+struct Decoder
+{	// Reflect element <Decoder>
+	char friendlyName[MAX_PATH]; // UTF-8
+	char id[40]; // GUID representing decoder's id
+	char ver[10]; // Version number
+};
+
+///////////////////////////////////////////////////////////////////////////
+
+class CConfig
+{
+public:
+	CConfig(void);
+	virtual ~CConfig(void);
+	bool LoadConfigFile(LPCWSTR FileName);
+	bool ExistCaptureEP(void);
+	bool GetFirstCaptureEP(const CaptureEndPoint * data);
+	bool GetSelectedCaptureEP(const CaptureEndPoint * data, bool Current=true);
+	bool GetCaptureEPbyIndex(const CaptureEndPoint * data, int index);
+	bool ExistDecoder(void);
+	bool GetDecoderbyIndex(const Decoder * data, int index);
+	bool GetSelectedDecoder(const Decoder * data);
+
+private:
+	bool IsIdentical(const char * utf8, LPCWSTR wStr);
+	bool IsIdentical(LPCWSTR wStr, const char * utf8);
+	bool GetCaptureEP(TiXmlElement * pElem, const CaptureEndPoint * data);
+	bool GetCaptureEPbyId(const CaptureEndPoint * data, const char * id);
+	bool GetDecoder(TiXmlElement * pElem, const Decoder * data);
+	bool GetDecoderbyId(const Decoder * data, const char * id);
+
+
+private:
+	TiXmlHandle m_hRoot;
+	TiXmlHandle m_hDoc;
+	LPWSTR		m_XmlPath;
+};
+
--- SPP_NG/branches/x00/Control/StateMachine.cpp
+++ SPP_NG/branches/x00/Control/StateMachine.cpp
@@ -1,5 +1,6 @@
 // This is the Control Unit State Machine
 #include "stdafx.h"
+#include "config.h"
 #include "control.h"
 using namespace std;
 #include "StateMachine.h"
@@ -97,100 +98,34 @@ HRESULT CStateMachine::LoadConfigFromFile(LPCWSTR FileName)
 // Given XML configuration file the function read the file (Using TinyXML)
 // Then updates state machine state and SPP configuration structure
 {
-	FILE * f;
-	TiXmlElement* pElem;
-	TiXmlHandle hRoot(0);
 	MessageMap  messages;
+	CaptureEndPoint EndPoint;
+	Decoder dcdr;
 
-	errno_t err = _wfopen_s(&f, FileName, L"r+b, ccs=UTF-8");
-	if (err)
+	if (m_config.LoadConfigFile(FileName))
+	{
+		bool found = m_config.ExistDecoder();
+		found = m_config.GetDecoderbyIndex(&dcdr,0);
+		found = m_config.GetDecoderbyIndex(&dcdr,1);
+		found = m_config.GetDecoderbyIndex(&dcdr,2);
+		found = m_config.GetSelectedDecoder(&dcdr);
+
+		found = m_config.GetSelectedCaptureEP(&EndPoint);
+		found = m_config.GetSelectedCaptureEP(&EndPoint, false);
+		bool existEP = m_config.ExistCaptureEP();
+		bool FirstEP = m_config.GetFirstCaptureEP(&EndPoint);
+		FirstEP = m_config.GetCaptureEPbyIndex(&EndPoint, 0);
+		FirstEP = m_config.GetCaptureEPbyIndex(&EndPoint, 1);
+		FirstEP = m_config.GetCaptureEPbyIndex(&EndPoint, 2);
+		FirstEP = m_config.GetCaptureEPbyIndex(&EndPoint, 3);
+		FirstEP = m_config.GetCaptureEPbyIndex(&EndPoint, 4);
+		FirstEP = m_config.GetCaptureEPbyIndex(&EndPoint, 5);
+		return S_OK;
+	}
+	else
 		return S_FALSE;
 
-	// Open & load XML file with TunyXML 
-	HRESULT hr = S_OK;
-	TiXmlDocument * doc = new(TiXmlDocument);
-	bool loaded = doc->LoadFile(f, TIXML_ENCODING_UTF8);
-	if (!loaded)
-	{
-		const char * desc = doc->ErrorDesc();
-		hr = ERROR_OPEN_FAILED;
-		goto end;
-	};
 
-	TiXmlHandle hDoc(doc);
-
-	pElem=hDoc.FirstChildElement().Element();
-	// should always have a valid root but handle gracefully if it does
-	if (!pElem) 
-	{
-		hr = ERROR_OPEN_FAILED;
-		goto end;
-	};
-
-	// Rootname is expected to be "SmartPropoPlus"
-	const char * root_name = pElem->Value();
-	if(!root_name)
-	{
-		hr = ERROR_OPEN_FAILED;
-		goto end;
-	};
-
-	if (!IsIdentical(XML_ROOT_ELEM, root_name))
-	{
-		hr = GetLastError();
-		goto end;
-	};
-
-
-	// OK - Rootname is indeed "SmartPropoPlus" 
-	// save this for later
-	hRoot=TiXmlHandle(pElem);
-
-	/////////////// Now get capture endpoints ////////
-	pElem=hRoot.FirstChild( "Audio" ).FirstChild().Element();
-	for( pElem; pElem; pElem=pElem->NextSiblingElement())
-	{
-		const char *pKey=pElem->Value(); // <CaptureEndpoint></CaptureEndpoint>
-		if (IsIdentical(L"CaptureEndPoint", pKey))
-		{
-			if (pElem->FirstChildElement("id")) const char *id_text = pElem->FirstChildElement("id")->GetText(); // id
-			if (pElem->FirstChildElement("color")) const char *color_text = pElem->FirstChildElement("color")->GetText(); // jack colour
-			if (pElem->FirstChildElement("friendlyName")) const char *color_frnm = pElem->FirstChildElement("friendlyName")->GetText(); // Friendly Name
-		};
-		int dummy = 5;
-		//if (pKey && pText) 
-		//{
-		//	messages[pKey]=pText;
-		//}
-	}
-
-
-
-
-end:
-	delete(doc);
-	fclose(f);
-	return hr;
+	return S_OK;
 }
 
-bool CStateMachine::IsIdentical(LPCWSTR wStr, const char * utf8)
-{
-	if (!wStr || !utf8)
-		return false;
-
-	WCHAR w_utf8[MAX_PATH];
-	int mb = MultiByteToWideChar(CP_UTF8, 0, utf8, -1, w_utf8,0); 
-	mb = MultiByteToWideChar(CP_UTF8, 0, utf8, -1, w_utf8,mb); 
-	DWORD lasterr = GetLastError();
-	if (lasterr)
-		return false;
-
-	if (wcscmp(wStr, w_utf8))
-		return false;
-	else
-		return true;
-}
-bool CStateMachine::IsIdentical(const char * utf8, LPCWSTR wStr)
-{
-	return IsIdentical(wStr, utf8);
-}
