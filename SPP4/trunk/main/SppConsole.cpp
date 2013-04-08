3bb89a89908f0e95fc8de77251404b072922a3fd
shaul_ei <shaul_ei@31d0aad3-184c-4fc1-91d3-241c0838d686>
2013-04-08 19:47:12 +0000

    Nightly commit

--- SPP4/trunk/main/SppConsole.cpp
+++ SPP4/trunk/main/SppConsole.cpp
@@ -2,23 +2,58 @@
 //
 
 #include "stdafx.h"
+#include <Shellapi.h>
+#include "SmartPropoPlus.h"
 #include "SppConsole.h"
 #include "SppConsoleDlg.h"
 
 
 
 
-int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPTSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
 	// Read Command line
-	// TODO
+	// TODO: This is only an example of how to parse the command line
+	int argc = 0;
+	LPWSTR* argv = NULL;
+	argv = CommandLineToArgvW(lpCmdLine, &argc);
+	LocalFree(argv);
+	// Read Command line (End)
 
 	//Registers a system-wide messages:
 	// WM_INTERSPPCONSOLE - to ensure that it is a singleton.
 	// WM_INTERSPPAPPS - Messages to the user interface window.
+	TCHAR msg[MAX_MSG_SIZE];
+	WM_INTERSPPCONSOLE	= RegisterWindowMessage(INTERSPPCONSOLE);
+	if (!WM_INTERSPPCONSOLE)
+	{
+		_stprintf_s(msg, TEXT("wWinMain(): WM_INTERSPPCONSOLE = %d - cannot register window message INTERSPPCONSOLE"), MAX_MSG_SIZE, WM_INTERSPPCONSOLE);
+		::MessageBox(NULL,msg, TEXT("SmartPropoPlus Message") , MB_SYSTEMMODAL);
+		return -1;
+	};
+	WM_INTERSPPAPPS		= RegisterWindowMessage(INTERSPPAPPS);
+	if (!WM_INTERSPPAPPS)
+	{
+		_stprintf_s(msg, TEXT("wWinMain(): WM_INTERSPPAPPS = %d - cannot register window message INTERSPPAPPS"), MAX_MSG_SIZE, WM_INTERSPPAPPS);
+		::MessageBox(NULL,msg, TEXT("SmartPropoPlus Message") , MB_SYSTEMMODAL);
+		return -1;
+	};
+
+		
+	/* Test if another SPPConsole is running */
+	HANDLE hMuxex;
+	if (hMuxex=OpenMutex(MUTEX_ALL_ACCESS, TRUE, MUTXCONSOLE))
+	{	// another instance is already running and the second instance is NOT 
+		// launched iconified then Broadcast a message.
+		//TODO: if (!m_Iconified)
+			::PostMessage(HWND_BROADCAST, WM_INTERSPPCONSOLE, 0, 0);
+		return -2;
+	}
+	else
+		hMuxex = CreateMutex(NULL, FALSE, MUTXCONSOLE);
 
 	SppConsoleDlg Dialog = SppConsoleDlg(hPrevInstance);
 	//Dialog.Hide();
