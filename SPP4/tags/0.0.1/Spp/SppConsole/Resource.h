64beca942815aac3bb4c663c8df6741470cb337f
shaul_ei <shaul_ei@31d0aad3-184c-4fc1-91d3-241c0838d686>
2013-05-09 08:36:36 +0000

    SPP fully converted to VS2012 express (MFC-Clean). Fully Functional. No enhancements over SPP3 This is only the starting point for SPP4

---
+++ SPP4/tags/0.0.1/Spp/SppConsole/Resource.h
@@ -0,0 +1,33 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_ABOUTBOX                            103
+#define IDM_ABOUT                               104
+#define IDM_EXIT                                105
+#define IDI_SMALL                               108
+#define IDC_SPPCONSOLE                          109
+#define IDD_SPPDIAG                             111
+#define IDI_SPPCONSOLE                          114
+#define IDC_CH_MONITOR                          1001
+#define IDC_LIST_FILTERS                        1002
+#define IDS_APP_TITLE                           40000
+#define IDC_LIST_AUDIOSRC                       40009
+#define IDC_LIST_PCM                            40010
+#define IDC_LIST_PPM                            40012
+#define IDC_CH1                                 40013
+#define IDC_CH2                                 40014
+#define IDC_CH3                                 40015
+#define IDC_CH4                                 40016
+#define IDC_CH5                                 40017
+#define IDC_CH6                                 40018
+#define IDC_CH7                                 40019
+#define IDC_CH8                                 40020
+#define IDC_TXT_CH1                             40030
+#define IDC_TXT_CH2                             40031
+#define IDC_TXT_CH3                             40032
+#define IDC_TXT_CH4                             40033
+#define IDC_TXT_CH5                             40034
+#define IDC_TXT_CH6                             40035
+#define IDC_TXT_CH7                             40036
+#define IDC_TXT_CH8                             40037
