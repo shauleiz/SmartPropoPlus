6b87ea7904c4caf9994a952b6a1e70ebfdbf7f1e
Shaul <shauleiz@users.noreply.github.com>
2013-11-27 09:06:01 +0000

    Work on Button Image - Some code added - problem with tool tips

--- SPP4/UITester/SppUI/WpfCtrlWin/JoyButton.xaml.cs
+++ SPP4/UITester/SppUI/WpfCtrlWin/JoyButton.xaml.cs
@@ -22,5 +22,40 @@ namespace WpfCtrlWin
         {
             InitializeComponent();
         }
+
+        private bool _pressed = false;
+        private bool _mapped = false;
+       private Brush _bgPressedBrush = new SolidColorBrush(Colors.Red);
+        private Brush _bgReleasedBrush = new SolidColorBrush(Colors.LightGray);
+        private Brush _bgUnMappedBrush = new SolidColorBrush(Colors.Transparent);
+
+        public Brush BgColor { set { BackGround.Fill = value; } get { return BackGround.Fill; } }
+        public String Number { set { ButtonNumber.Content = value; } get { return ButtonNumber.Content as string; } }
+        public bool Pressed
+        {
+            set {
+                _pressed = value;
+                if (_pressed)
+                    BgColor = _bgPressedBrush;
+                else
+                    BgColor = _bgReleasedBrush;
+            }
+            get { return _pressed;}
+        } //  Pressed
+
+
+        public bool Mapped
+        {
+            set
+            {
+                _mapped = value;
+                if (_mapped)
+                    Pressed = _pressed;
+                else
+                    BgColor = _bgUnMappedBrush;
+            }
+            get { return _mapped; }
+        } // Mapped
+
     }
 }
