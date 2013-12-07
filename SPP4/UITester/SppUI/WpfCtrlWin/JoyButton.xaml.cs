25576d7649c9946460e03b4cdf40b7d3943e87a0
Shaul <shauleiz@users.noreply.github.com>
2013-12-07 14:30:04 +0000

    Added IsEnabled to ChannelCtrl user control Implemented for vJoy buttons Functional: LastButton, IsMapped, IsPressed

--- SPP4/UITester/SppUI/WpfCtrlWin/JoyButton.xaml.cs
+++ SPP4/UITester/SppUI/WpfCtrlWin/JoyButton.xaml.cs
@@ -23,39 +23,126 @@ namespace WpfCtrlWin
             InitializeComponent();
         }
 
+        // Defaults
         private bool _pressed = false;
         private bool _mapped = false;
-       private Brush _bgPressedBrush = new SolidColorBrush(Colors.Red);
+        private bool _enabled = false;
+        private Brush _bgPressedBrush = new SolidColorBrush(Colors.Red);
         private Brush _bgReleasedBrush = new SolidColorBrush(Colors.LightGray);
-        private Brush _bgUnMappedBrush = new SolidColorBrush(Colors.Transparent);
+        private Brush _bgUnMappedBrush = new SolidColorBrush(Colors.Wheat);
+        private Brush _bgDisabledBrush = new SolidColorBrush(Colors.Yellow);
+        private Brush _ringStrokeBrush = new SolidColorBrush(Colors.Black);
+        private Brush _ringDisabledBrush = new SolidColorBrush(Colors.Transparent);
 
-        public Brush BgColor { set { BackGround.Fill = value; } get { return BackGround.Fill; } }
+        // Override defaults
+        public Brush BgPressedBrush {set { _bgPressedBrush = value; } get { return _bgPressedBrush;}}
+        public Brush BgReleasedBrush { set { _bgReleasedBrush = value; } get { return _bgReleasedBrush;}}
+        public Brush BgUnMappedBrush { set { _bgUnMappedBrush = value; } get { return _bgUnMappedBrush; } }
+        public Brush BgDisabledBrush { set { _bgDisabledBrush = value; } get { return _bgDisabledBrush; } }
+
+        // Internal properties
+
+        /// <summary>
+        /// Compute value of background brush
+        /// </summary>
+        private Brush BgColor() 
+        { 
+                if (!_enabled)
+                    BackGround.Fill = _bgDisabledBrush;
+                else
+                {
+                    if (!_mapped)
+                        BackGround.Fill = _bgUnMappedBrush;
+                    else
+                    {
+                        if (_pressed)
+                            BackGround.Fill = _bgPressedBrush;
+                        else
+                            BackGround.Fill = _bgReleasedBrush;
+                    }
+                }
+
+                return BackGround.Fill;
+        }
+
+
+        private Brush RingStroke { set { OuterRing.Stroke = value; } get { return OuterRing.Stroke; } }
+
         public String Number { set { ButtonNumber.Content = value; } get { return ButtonNumber.Content as string; } }
-        public bool Pressed
-        {
-            set {
-                _pressed = value;
-                if (_pressed)
-                    BgColor = _bgPressedBrush;
-                else
-                    BgColor = _bgReleasedBrush;
-            }
-            get { return _pressed;}
-        } //  Pressed
 
 
-        public bool Mapped
+#if false
+
+        /// <summary>
+        /// IsEnabled - Represents an existing vJoy device button
+        /// Value is changed when device changes
+        /// Other properties ('Mapped', 'Pressed') are meaningful only when button is enabled
+        /// </summary>
+        public new bool IsEnabled
         {
-            set
+            get { return (bool)GetValue(IsEnabledProperty); }
+            set { SetValue(IsEnabledProperty, value); }
+        }
+
+        public new static readonly DependencyProperty IsEnabledProperty =
+            DependencyProperty.Register("IsEnabled", typeof(bool), typeof(JoyButton), new UIPropertyMetadata(false, (o, e) =>
             {
-                _mapped = value;
-                if (_mapped)
-                    Pressed = _pressed;
+                ((JoyButton)o)._enabled = (bool)e.NewValue;
+                ((JoyButton)o).BackGround.Fill = ((JoyButton)o).BgColor();
+            }));
+        
+#endif
+        /// <summary>
+        /// IsMapped - True if an input channel is mapped to an enabled vJoy device button
+        /// Property 'Pressed' is meaningful only when button is Mapped
+        /// </summary>
+        public  bool IsMapped
+        {
+            get { return (bool)GetValue(IsMappedProperty); }
+            set { SetValue(IsMappedProperty, value); }
+        }
+
+        public  static readonly DependencyProperty IsMappedProperty =
+            DependencyProperty.Register("IsMapped", typeof(bool), typeof(JoyButton), new UIPropertyMetadata(false, (o, e) =>
+            {
+                ((JoyButton)o)._mapped = (bool)e.NewValue;
+                ((JoyButton)o).BackGround.Fill = ((JoyButton)o).BgColor(); 
+            }));
+
+        /// <summary>
+        /// IsPressed - True if an input button is 'Pressed' (Only Mapped buttons can be 'Pressed')
+        /// </summary>
+        public  bool IsPressed
+        {
+            get { return (bool)GetValue(IsPressedProperty); }
+            set { SetValue(IsPressedProperty, value); }
+        }
+
+        public  static readonly DependencyProperty IsPressedProperty =
+            DependencyProperty.Register("IsPressed", typeof(bool), typeof(JoyButton), new UIPropertyMetadata(false, (o, e) =>
+            {
+                ((JoyButton)o)._pressed = (bool)e.NewValue;
+                ((JoyButton)o).BackGround.Fill = ((JoyButton)o).BgColor(); 
+            }));
+
+        public int LastButton
+        {
+            get { return (int)GetValue(LastButtonProperty); }
+            set { SetValue(LastButtonProperty, value); }
+        }
+
+        public static readonly DependencyProperty LastButtonProperty =
+            DependencyProperty.Register("LastButton", typeof(int), typeof(JoyButton), new UIPropertyMetadata(0, (o, e) =>
+            {
+                var result = 0;
+                if (int.TryParse(((JoyButton)o).Number, out result))
+                    ((JoyButton)o)._enabled = ((int)e.NewValue >= Convert.ToInt32(((JoyButton)o).Number));
                 else
-                    BgColor = _bgUnMappedBrush;
-            }
-            get { return _mapped; }
-        } // Mapped
+                    ((JoyButton)o)._enabled = false;
+
+                ((JoyButton)o).BackGround.Fill = ((JoyButton)o).BgColor();
+            }));
+
 
     }
 }
