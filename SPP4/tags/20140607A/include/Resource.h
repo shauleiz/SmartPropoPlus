5df5ea9646c575219255183bda2ea6628da07b29
Shaul <shauleiz@users.noreply.github.com>
2014-06-07 08:42:37 +0000

    Release 20140607

---
+++ SPP4/tags/20140607A/include/Resource.h
@@ -0,0 +1,17 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define FILEVER "0,4,0,0"
+
+#ifdef X64
+#define PRODVER "0,4,0,0 (x64) Build 20140607"
+#else
+#define PRODVER "0,4,0,0 (x86) Build 20140607"
+#endif
+
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#include "..\SppUI\Resource.h"
