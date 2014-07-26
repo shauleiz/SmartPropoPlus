a5f40669813edbbe122758d0354c49c5ae9a977c
Shaul <shauleiz@users.noreply.github.com>
2014-07-26 07:20:38 +0000

    RC1

---
+++ SPP4/tags/20140726AA/JoyMonitor/Resource.h
@@ -0,0 +1,32 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_ABOUTBOX                            103
+#define IDM_ABOUT                               104
+#define IDM_EXIT                                105
+#define IDI_JOYMONITOR                          107
+#define IDC_JOYMONITOR                          109
+#define IDD_JOY                                 111
+#define IDB_GREENDOT                            118
+#define IDB_GREYDOT                             120
+#define IDB_REDDOT                              122
+#define IDS_APP_TITLE                           40000
+#define IDC_TXT_X                               40035
+#define IDC_TXT_Y                               40037
+#define IDC_TXT_Z                               40039
+#define IDC_TXT_RX                              40041
+#define IDC_TXT_RY                              40043
+#define IDC_TXT_RZ                              40045
+#define IDC_TXT_SL0                             40047
+#define IDC_TXT_SL1                             40049
+#define IDC_VJOY_AXES                           40050
+#define IDC_VJOY_DEVICE                         40068
+#define IDC_X                                   40100
+#define IDC_Y                                   40101
+#define IDC_Z                                   40102
+#define IDC_RX                                  40103
+#define IDC_RY                                  40104
+#define IDC_RZ                                  40105
+#define IDC_SL0                                 40106
+#define IDC_SL1                                 40107
---
+++ SPP4/tags/20140726AA/SppUI/SppTabAdv.cpp
@@ -0,0 +1,197 @@
+#include "stdafx.h"
+#include "Windowsx.h"
+#include "Commctrl.h"
+#include "resource.h"
+#include "WinMessages.h"
+#include "SmartPropoPlus.h"
+#include "SppTabAdv.h"
+
+// GLobals
+INT_PTR CALLBACK	MsgHndlTabAdvDlg(HWND, UINT, WPARAM, LPARAM);
+
+static const int g_Controls[] = {
+		/* Advanced Tab (IDD_ADVANCED ) */
+		IDC_CH_LOG, IDC_CH_INSIG, IDC_CH_PULSE, IDC_PLS_SCOPE, IDC_BTN_RESET,
+	};
+
+
+SppTabAdv::SppTabAdv(void)
+{
+}
+
+
+SppTabAdv::~SppTabAdv(void)
+{
+}
+
+
+SppTabAdv::SppTabAdv(HINSTANCE hInstance, HWND TopDlgWnd) :  SppTab( hInstance,  TopDlgWnd,  IDD_ADVANCED, MsgHndlTabAdvDlg)
+{
+}
+
+// Tell the parent window (Main application)
+// to stop/start displaying pulse scope
+void  SppTabAdv::PulseScope(WORD cb)
+{
+	// Get data
+	HWND hMonitorChCB = GetDlgItem(m_hDlg,  cb);
+
+	// Pass request
+	int start = Button_GetCheck(hMonitorChCB);
+	SendMessage(m_TopDlgWnd, WMSPP_DLG_PLSSCOP , start, 0);
+}
+
+// Tell the parent window (Main application)
+// to show/hide log window
+void  SppTabAdv::ShowLogWindow(WORD cb)
+{
+	// Get data
+	HWND hLogChCB = GetDlgItem(m_hDlg,  cb);
+	int show = Button_GetCheck(hLogChCB);
+	SendMessage(m_TopDlgWnd, WMSPP_DLG_LOG , show, 0);
+}
+
+
+// Reset button was pressed pass it to the parent
+void  SppTabAdv::ResetRequest(void)
+{
+	SendMessage(m_TopDlgWnd, WMSPP_DLG_RST , 0, 0);
+}
+
+void SppTabAdv::Reset()
+{
+	//Clear check box IDC_PLS_SCOPE
+	HWND hLogChCB = GetDlgItem(m_hDlg,  IDC_PLS_SCOPE);
+	Button_SetCheck(hLogChCB, BST_UNCHECKED);
+ 
+	PulseScope(IDC_PLS_SCOPE);
+}
+
+void  SppTabAdv::RecordPulse(WORD cb)
+{
+	// Get data
+	HWND hRecordPulseChCB = GetDlgItem(m_hDlg,  cb);
+	int record = Button_GetCheck(hRecordPulseChCB);
+	SendMessage(m_TopDlgWnd, WMSPP_DLG_PULSE , record, 0);
+
+}
+
+void  SppTabAdv::RecordInSignal(WORD cb)
+{
+	// Get data
+	HWND hRecordInSignalChCB = GetDlgItem(m_hDlg,  cb);
+	int record = Button_GetCheck(hRecordInSignalChCB);
+	SendMessage(m_TopDlgWnd, WMSPP_DLG_INSIG , record, 0);
+
+}
+
+/*
+	Called every time mouse hovers over a control that was previously registered for tool tip
+	Registration was done in CreateToolTip()
+	The Control ID (CtrlId) of the control is extracted from the input 'param' 
+	The correct text is displayed according to the Control ID
+*/
+void SppTabAdv::UpdateToolTip(LPVOID param)
+{
+	LPNMTTDISPINFO lpttt = (LPNMTTDISPINFO)param;
+	TCHAR ControlText[MAX_MSG_SIZE] ={0};
+	TCHAR TitleText[MAX_MSG_SIZE] ={0};
+	int ControlTextSize = 0;
+
+	// Since the id field of the control in the tooltip was defined as a handle - it has to be converted back
+	int CtrlId = GetDlgCtrlID((HWND)lpttt->hdr.idFrom);
+
+	// Handle to the tooltip window
+	HWND hToolTip = lpttt->hdr.hwndFrom;
+
+	switch (CtrlId) // Per-control tooltips
+	{
+	case IDC_CH_LOG:
+		DisplayToolTip(lpttt, IDS_I_CH_LOG, IDS_T_CH_LOG);
+		break;
+
+	case IDC_CH_INSIG:
+		DisplayToolTip(lpttt, IDS_I_CH_INSIG, IDS_T_CH_INSIG);
+		break;
+
+	case IDC_CH_PULSE:
+		DisplayToolTip(lpttt, IDS_I_CH_PULSE, IDS_T_CH_PULSE);
+		break;
+
+	case IDC_PLS_SCOPE:
+		DisplayToolTip(lpttt, IDS_I_PLS_SCOPE, IDS_T_PLS_SCOPE);
+		break;
+
+	case IDC_BTN_RESET:
+		DisplayToolTip(lpttt, IDS_I_BTN_RESET, IDS_T_BTN_RESET);
+		break;
+
+	default:
+		DisplayToolTip(lpttt, IDS_W_NOT_IMP, L"OOOPS", TTI_WARNING);
+		break;
+	}
+
+}
+
+
+INT_PTR CALLBACK MsgHndlTabAdvDlg(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	static SppTabAdv * DialogObj = NULL;
+
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		DialogObj = (SppTabAdv *)lParam;
+		DialogObj->SetPosition(hDlg) ;
+		DialogObj->CreateToolTip(hDlg, g_Controls, sizeof(g_Controls)/sizeof(int)); // Initialize tooltip object
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDC_PLS_SCOPE)
+		{
+			DialogObj->PulseScope(LOWORD(wParam));
+			break;
+		};
+
+		if (LOWORD(wParam) == IDC_CH_LOG)
+		{
+			DialogObj->ShowLogWindow(LOWORD(wParam));
+			break;
+		};
+
+		if (LOWORD(wParam) == IDC_CH_INSIG)
+		{
+			DialogObj->RecordInSignal(LOWORD(wParam));
+			break;
+		};
+
+		if (LOWORD(wParam) == IDC_CH_PULSE)
+		{
+			DialogObj->RecordPulse(LOWORD(wParam));
+			break;
+		};
+
+		if (LOWORD(wParam) == IDC_BTN_RESET  && HIWORD(wParam) == BN_CLICKED )
+		{
+			DialogObj->ResetRequest();
+			break;
+		};
+
+	break;
+
+	case WM_NOTIFY:
+		// Tooltips
+		if (((LPNMHDR)lParam)->code == TTN_GETDISPINFO)
+		{
+			DialogObj->UpdateToolTip((LPVOID)lParam);
+			return  (INT_PTR)TRUE;
+		};
+
+	default:
+		break;
+
+	};
+
+	return (INT_PTR)FALSE;
+}
+
