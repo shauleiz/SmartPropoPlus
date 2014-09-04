f78fe11dd90e6962edb3e1e7c11348f484f33d45
shaul_ei <shaul_ei@31d0aad3-184c-4fc1-91d3-241c0838d686>
2014-09-04 18:43:21 +0000

    First production release for 4.0.0 It's build 20140904

---
+++ SPP4/tags/4.0.0Rel01/SppProcess/stdafx.h
@@ -0,0 +1,29 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+//#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <time.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <Shellapi.h>
+#include <mutex>
+#include <array>
+#include <Resource.h>
+#include <Strsafe.h>
+
+
+// TODO: reference additional headers your program requires here
+using namespace std;
---
+++ SPP4/tags/4.0.0Rel01/SppUI/SppTabAdv.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "spptab.h"
+class SppTabAdv : public SppTab
+
+{
+public:
+	SppTabAdv(void);
+	virtual ~SppTabAdv(void);
+	SppTabAdv(HINSTANCE hInstance, HWND TopDlgWnd);
+
+	void PulseScope(WORD cb);
+	void ShowLogWindow(WORD cb);
+	void RecordInSignal(WORD cb);
+	void RecordPulse(WORD cb);
+	void UpdateToolTip(LPVOID param);
+	void ResetRequest(void);
+	void Reset();
+};
+
