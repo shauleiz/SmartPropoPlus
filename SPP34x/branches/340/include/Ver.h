557b41dffcbb2b7c9222ad2d8c43f6681b552b67
Shauleiz <shaul.eiz@gmail.com>
2015-11-15 22:12:07 +0200

    Starting Branch

--- SPP34x/branches/340/include/Ver.h
+++
@@ -1,9 +0,0 @@
-// Resources
-#define VER_DOTS	"3.4.0"
-#define VER_COMMAS	"0, 3, 4, 0"
-#define	VER_COMMA	0, 3, 4, 0
-
-// C/C++
-#define	VER_DLL		0x00030400 /* DLL File Version */
-#define	VER_GUI		0x00030400 /* GUI File Version */
-
--- SPP_NG/branches/x00/Control/Control.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "resource.h"
-
-void OpenConfigurationWindow(HWND hWnd);
-void NotificationGraphics(HWND hWnd, DWORD_PTR state);
-bool PopulateUI_Jacks(HWND hWnd);
