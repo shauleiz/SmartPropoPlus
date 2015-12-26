9b7a78991f1297d2cc057055c6497419a5f4643c
Shauleiz <shaul.eiz@gmail.com>
2015-12-26 22:43:28 +0200

    Improved batch file that builds SPP

--- SPP4/trunk/Spp/AutoBld.bat
+++ SPP4/trunk/Spp/AutoBld.bat
@@ -1,7 +1,27 @@
 @ECHO ON
+
+SET VS=14.0
+SET BUILDER=%ProgramFiles(x86)%\MSBuild\%VS%\Bin\MSBuild.exe
+
+REM Setting the Path and Environment Variables for Command-Line Builds
 CALL "%VS110COMNTOOLS%"..\..\vc\bin\vcvars32.bat
-CALL msbuild  /t:Rebuild /p:Configuration=Release;Platform=Win32
-CALL msbuild  /t:Rebuild /p:Configuration=Release;Platform=x64 
+
+
+:build32
+echo Building SmartPropoPlus (x86)
+"%BUILDER%"  spp.sln  /t:rebuild /p:Platform=Win32;Configuration=Release
+set BUILD_STATUS=%ERRORLEVEL%
+if not %BUILD_STATUS%==0 goto fail
+
+:build64
+echo Building SmartPropoPlus (x64)
+"%BUILDER%"  spp.sln  /t:rebuild /p:Platform=x64;Configuration=Release
+set BUILD_STATUS=%ERRORLEVEL%
+if not %BUILD_STATUS%==0 goto fail
+
+REM Create Installer
 CALL "%ProgramFiles(x86)%\Inno Setup 5\ISCC" Installer\Installer.iss"
 PAUSE
 
+:fail
+exit /b 1
